2639858f9b0351a470d04de94566c2d7c4673739
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-04 23:54:16 +0200

    add tabs component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Tooltip } from './ui/tooltip';
 import { Dropdown } from './ui/dropdown';
+import { Tabs } from './ui/tabs';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 
@@ -10,3 +11,6 @@ dropdowns.forEach(dropdown => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
