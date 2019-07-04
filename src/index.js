157fbfb7f63f6f7e05b7f31933b1308dcb4babe5
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-04 23:34:07 +0200

    add dropdown component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { Tooltip } from './ui/tooltip';
+import { Dropdown } from './ui/dropdown';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 
 tooltip.init();
+
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
---
+++ src/ui/dropdown.js
@@ -0,0 +1,13 @@
+export class Dropdown {
+  constructor(container) {
+    this.container = container;
+    this.trigger = container.querySelector('.trigger');
+    this.content = container.querySelector('.content');
+  }
+  init() {
+    this.trigger.addEventListener('click', () => {
+      this.trigger.classList.toggle('active');
+      this.content.classList.toggle('active');
+    })
+  }
+}
